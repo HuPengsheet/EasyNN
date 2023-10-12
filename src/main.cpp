d6b59d3f2c080229ffde5b7a6c8e641b28403642
HuPengsheet <1554414201@qq.com>
2023-10-13 01:01:39 +0800

    remmove main

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<iostream>
+#include"ir.h"
+
+
+
+// int main()
+// {   
+
+//     const char * param_path = "src/res18.pnnx.param";
+//     const char * bin_path = "src/res18.pnnx.bin";
+//     pnnx::Graph graph;
+//     graph.load(param_path,bin_path);
+
+//     forward(graph,49);
+//     printf("hello world\n");
+//     return 0;
+// }
