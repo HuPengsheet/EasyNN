ab298b99eb5a032095f2d6e03c71bea08c4f3abc
HuPengsheet <1554414201@qq.com>
2023-11-04 23:18:34 +0800

    maxpool op

--- src/layers/maxpool.h
+++ src/layers/maxpool.h
@@ -11,6 +11,17 @@ class MaxPool: public Layer
 public:
     MaxPool();
     virtual int forward(const Mat& input,Mat& output,const Optional& op);
+    virtual int loadParam(std::map<std::string, pnnx::Parameter>& params);
+    void copy_make_border_image(const Mat& input,Mat& input_pad);
+public:
+
+    bool ceil_mode ;
+    bool return_indices;
+    std::vector<int> padding;     //type 5
+    std::vector<int> dilation;    //type 5
+    std::vector<int> kernel_size; //type 5
+    std::vector<int> stride;      //type 5
+    
 };
 
 }//namespace
