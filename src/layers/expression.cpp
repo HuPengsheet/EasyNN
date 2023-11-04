03b231e951e44b8caf35c5cd5ecd1d181480737b
HuPengsheet <1554414201@qq.com>
2023-11-04 23:15:30 +0800

    add expression op

--- src/layers/expression.cpp
+++ src/layers/expression.cpp
@@ -7,10 +7,62 @@ namespace easynn{
     {
         one_blob_only = false;
     }
+
     int Expression::forward(const std::vector<Mat>& input,std::vector<Mat>& output,const Optional& op)
     {
-        output[0]=input[0].clone();
-        std::cout<<"expression forward"<<std::endl;
+        Mat add_left = input[0];
+        Mat add_right = input[1];
+        
+        if(add_left.w!=add_right.w || add_left.h!=add_right.h || add_left.c!=add_right.c || add_left.d!=add_right.d)
+        {
+            printf("the shape is different, can not add \n");
+            return -1;
+        }
+
+        Mat& out = output[0];
+        int w=add_left.w;
+        int h=add_left.h;
+        int d=add_left.d;
+        int c=add_left.c;
+
+        if(add_left.dims == 1)
+            out.create(w);
+        if (add_left.dims == 2)
+            out.create(w, h);
+        else if (add_left.dims == 3)
+            out.create(w,h,c);
+        else if (add_left.dims == 4)
+            out.create(w,h,d,c);
+
+    for (int q=0; q<c; q++)
+    {
+        float* left_ptr = add_left.channel(q);
+        float* right_output = add_right.channel(q);
+        float* out_ptr = out.channel(q);
+
+        for (int z=0; z<d; z++)
+        {
+            for (int y=0; y<h; y++)
+            {
+                for (int x=0; x<w; x++)
+                {
+                    out_ptr[x]=left_ptr[x]+right_output[x];
+                }
+                left_ptr += w;
+                right_output += w;
+                out_ptr+=w;
+            }
+        }
     }
+        return 0;
+    }
+
+    int Expression::loadParam(std::map<std::string, pnnx::Parameter>& params)
+    {
+        expression=params["expr"].s;
+        return 0;
+    }
+        
+    
 
 }//namespace
--- src/layers/expression.h
+++ src/layers/expression.h
@@ -3,12 +3,17 @@
 
 #include"layer.h"
 #include"mat.h"
+
 namespace easynn{
 class Expression:public Layer
 {
 public:
     Expression();
     virtual int forward(const std::vector<Mat>& input,std::vector<Mat>& output,const Optional& op);
+    virtual int loadParam(std::map<std::string, pnnx::Parameter>& params);
+
+public:
+    std::string expression;
 };
 
 }//namespace
