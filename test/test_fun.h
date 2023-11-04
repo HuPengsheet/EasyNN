be5c0d589ea25fcf75b1700d0194906e25e3e457
HuPengsheet <1554414201@qq.com>
2023-11-04 23:19:32 +0800

    change test_fun

--- test/test_fun.h
+++ test/test_fun.h
@@ -35,6 +35,21 @@ void printMat(const easynn::Mat& m)
 }
 
 
+// int compareMat(const easynn::Mat& m1,const easynn::Mat& m2)
+// {
+//     if(m1.isEmpty()||m2.isEmpty())
+//     {
+//         //printf("mat is empty,con't compare\n");
+//         return -1;
+//     } 
+//     if(m1.c!=m2.c || m1.h!=m2.h || m1.w!=m2.w || m1.total()!=m2.total())
+//     {
+//         //printf("mat is shape is different,con't compare\n");
+//         return -1;
+//     }
+//     return !memcmp(m1.data, m2.data,m1.total());
+// }
+
 int compareMat(const easynn::Mat& m1,const easynn::Mat& m2)
 {
     if(m1.isEmpty()||m2.isEmpty())
@@ -47,7 +62,25 @@ int compareMat(const easynn::Mat& m1,const easynn::Mat& m2)
         //printf("mat is shape is different,con't compare\n");
         return -1;
     }
-    return !memcmp(m1.data, m2.data,m1.total());
+    for (int q=0; q<m1.c; q++)
+    {
+        float* ptr1 = m1.channel(q);
+        float* ptr2 = m2.channel(q);
+        for (int z=0; z<m1.d; z++)
+        {
+            for (int y=0; y<m1.h; y++)
+            {
+                for (int x=0; x<m1.w; x++)
+                {
+                    if(abs(ptr1[x]-ptr2[x])>1e-2)
+                        return -1;
+                }
+                ptr1 += m1.w;
+                ptr2 += m2.w;
+            }
+        }
+    }
+    return 0;
 }
 
 #endif
