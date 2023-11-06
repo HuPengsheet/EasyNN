3b5b4ddfb1ed063d79683d200b05f6f192096289
HuPengsheet <1554414201@qq.com>
2023-11-06 14:31:46 +0800

    upsample layers

--- src/layers/upsample.h
+++ src/layers/upsample.h
@@ -11,6 +11,11 @@ class Upsample: public Layer
 public:
     Upsample();
     virtual int forward(const Mat& input,Mat& output,const Optional& op);
+    virtual int loadParam(std::map<std::string, pnnx::Parameter>& params);
+public:
+    std::string mode;
+    std::vector<float> scale_factor; 
+
 };
 
 }//namespace
